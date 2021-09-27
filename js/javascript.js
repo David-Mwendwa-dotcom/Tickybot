f6104791e82708c35bdb5eef370f4f2a558e293e
David-Mwendwa-dotcom <mwendwadavid628@gmail.com>
2021-09-27 13:12:13 +0300

    main

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,19 @@
 function Top() {
-    var page = document.getElementById("mainHead");
-    page.scrollTop = 100;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
 
 function Dosignup() {
     location.replace("https://slack.com/oauth/authorize?client_id=39189849459.249480535796&scope=commands,bot,users%2Eprofile%3Aread,im%3Aread,chat%3Awrite%3Abot,team%3Aread")
+}
+
+function scrollToHeader() {
+    var newElement = document.getElementById();
+    if (newElement == "mainHead") {
+        newElement.scrollIntoView();
+    } else if (newElement == "About") {
+        newElement.scrollIntoView();
+    } else if (newElement == "Features") {
+        newElement.scrollIntoView();
+    }
 }
